bfcf9549728fdbe79785ab31293f57c25926a986
dalhun namgung <bnamgung@gmail.com>
2018-02-19 16:21:21 -0800

    working on

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -13,54 +13,52 @@ const {EnvironmentPlugin, DefinePlugin} = require('webpack');
 let production = process.env.NODE_ENV === "production";
 
 let plugins = [
-    new HTMLPlugin({
-        template: `${__dirname}/src/index.html`
-    }),
-    new ExtractPlugin('bundle.[hash].css'),
-    new EnvironmentPlugin(['NODE_ENV']),
-    new DefinePlugin({
-        '__GOOGLE_CLIENT_ID__': JSON.stringify(process.env.GOOGLE_CLIENT_ID),
-        '__GOOGLE_CLIENT_SECRET__': JSON.stringify(process.env.GOOGLE_CLIENT_SECRET),
-        '__AUTH_URL__': JSON.stringify(process.env.AUTH_URL),
-        '__API_URL__': JSON.stringify(process.env.API_URL),
-        '__DEBUG__': JSON.stringify(! production)
-    })
+  new HTMLPlugin({
+    template: `${__dirname}/src/index.html`
+  }),
+  new ExtractPlugin('bundle.[hash].css'),
+  new EnvironmentPlugin(['NODE_ENV']),
+  new DefinePlugin({
+    '__GOOGLE_CLIENT_ID__': JSON.stringify(process.env.GOOGLE_CLIENT_ID),
+    '__GOOGLE_CLIENT_SECRET__': JSON.stringify(process.env.GOOGLE_CLIENT_SECRET),
+    '__AUTH_URL__': JSON.stringify(process.env.AUTH_URL),
+    '__API_URL__': JSON.stringify(process.env.API_URL),
+    '__DEBUG__': JSON.stringify(! production)
+  })
 ];
 
 module.exports = {
 
-    plugins,
+  plugins,
 
-    // Load this and everythning it cares about
-    entry: `${__dirname}/src/main.js`,
+  // Load this and everythning it cares about
+  entry: `${__dirname}/src/main.js`,
 
-    devServer: {
-        historyApiFallback:true
-    },
+  devServer: {
+    historyApiFallback:true
+  },
 
-    devtool: 'source-map',
+  devtool: 'source-map',
 
-    // Stick it into the "path" folder with that file name
-    output: {
-        filename: 'bundle.[hash].js',
-        path: `${__dirname}/build`
-    },
+  // Stick it into the "path" folder with that file name
+  output: {
+    filename: 'bundle.[hash].js',
+    path: `${__dirname}/build`
+  },
 
-    module: {
-        rules: [
-            // If it's a .js file not in node_modules, use the babel-loader
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            // If it's a .scss file
-            {
-                test: /\.scss$/,
-                loader : 'style-loader!css-loader!sass-loader'
-            },
-
-        ]
-    }
-
+  module: {
+    rules: [
+      // If it's a .js file not in node_modules, use the babel-loader
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      // If it's a .scss file
+      {
+        test: /\.scss$/,
+        loader : 'style-loader!css-loader!sass-loader'
+      },
+    ]
+  }
 }
